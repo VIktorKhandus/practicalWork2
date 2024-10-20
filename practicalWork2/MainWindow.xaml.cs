030f43498ffc41cb608b987f6506b692a3d2a158
KhandusVIktor <is25khandusvs@artcollege.ru>
2024-10-20 15:16:49 +0300

    Готово

--- practicalWork2/MainWindow.xaml.cs
+++ practicalWork2/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using practicalWork2.DataModel;
+using System.Windows.Threading;
 
 namespace practicalWork2
 {
@@ -23,6 +26,65 @@ namespace practicalWork2
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private Model1 model = new Model1();
+        private int Skip = 0;
+        private int Take = 25;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(LoadData);
+        }
+
+        private void LoadData(object sender)
+        {
+        
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)(() =>
+            { 
+            LabelLoad.Visibility = Visibility.Visible;
+                wrapPanelWeather.Children.Clear();
+            }));
+            foreach (Weather weather in model.Weather.ToList().Skip(Skip).Take(Take))
+            {
+                Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)(() =>
+                {
+                    UserControlWeather controlWeather = new UserControlWeather(weather);
+                    wrapPanelWeather.Children.Add(controlWeather);
+                }));
+            }
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)(() =>
+            {
+                LabelLoad.Visibility = Visibility.Collapsed;
+            }));
+        }
+
+        private void buttonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (Skip > Take && Skip !=0)
+            {
+                Skip += Take;
+                ThreadPool.QueueUserWorkItem(LoadData);
+            }
+            else if (Skip != 0)
+            {
+                Skip = 0;
+                ThreadPool.QueueUserWorkItem(LoadData);
+            }
+        }
+
+        private void buttonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            int Summ = Skip + Take;
+            if (model.Weather.ToList().Count > Summ)
+            {
+                Skip += Take;
+                ThreadPool.QueueUserWorkItem(LoadData);
+            }
+            else if (model.Weather.ToList().Count < Skip)
+            {
+                Skip += model.Weather.ToList().Count - Skip;
+                ThreadPool.QueueUserWorkItem(LoadData);
+            }
         }
     }
 }
